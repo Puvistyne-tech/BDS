d6b362afedc000b33222d76208c5117964dbbb19
puvistyne <56639092+Puvistyne-tech@users.noreply.github.com>
2022-02-05 22:19:06 +0100

    Add files via upload

---
+++ js/myJQuery.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".myButtons").click(function () {
+        let input = $(this).text()
+        createMeElement(input)
+    });
+});
+
+const createMeElement = (input) => {
+    let text = $("<div></div>").text("You clicked : " + input)
+    text
+        .addClass("text-center")
+        .addClass("border")
+        .addClass("border-danger")
+        .addClass("rounded")
+        .addClass("p-3")
+        .addClass("m-2")
+    $("#display").children().append(text)
+}
